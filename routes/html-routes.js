77227900cce15355ceac1139b1ba72eaebd4f8b1
Liam Bradley <liamhbradley11@gmail.com>
2020-10-15 23:50:25 -0400

    notes can save

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,19 +3,13 @@ var path = require("path");
 module.exports = function(app) {
  
     app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "./public/notes.html"));
-  });
-
-
-  //links to css file
-    app.get("/styles", function(req, res) {
-      res.sendFile(path.join(__dirname, "./public/assets/css/style.css"));
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
   });
 
 
   //default to the homepage
     app.get("*", function(req, res) {
-        res.sendFile(path.join(__dirname, "./public/index.html"));
+        res.sendFile(path.join(__dirname, "../public/index.html"));
     });
 
 }
--- server.js
+++ server.js
@@ -1,21 +1,23 @@
 //dependencies
-const express = require("express");
-const fs = require("fs");
-const path = require("path");
+var express = require("express");
+// const path = require("path");
+
 
 //initialize express
-const app = express();
-const PORT = process.env.PORT || 3000;
+var app = express();
+var PORT = process.env.PORT || 3000;
+
 
 //seting up data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(__dirname));
+app.use(express.static("public"));
 
 
 //route
 require("./routes/api-routes")(app);
 require("./routes/html-routes")(app);
+
 
 //listen on port
 app.listen(PORT, function() {
